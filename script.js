991f76fda7dbb2ff57464095636610edc9808d68
abyy00001@stud.uni-saarland.de <abhikumar271188@gmail.com>
2022-07-30 18:23:17 +0200

    commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let currentScore = 0;
+let activePlayer = 0;
 
 document.querySelector('#score--0').textContent = 0;
 document.getElementById('score--1').textContent = 0;
@@ -15,6 +16,28 @@ document.querySelector('.btn--roll').addEventListener('click', function () {
   document.querySelector('.dice').src = `dice-${randomNumber}.png`;
   document.querySelector('.dice').classList.remove('hidden');
 
-  currentScore += randomNumber;
-  document.getElementById('current--0').textContent = currentScore;
+  //Function for swithcing the player
+  function switchPlayer() {
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    document.querySelector('.player--0').classList.toggle('player--active');
+    document.querySelector('.player--1').classList.toggle('player--active');
+  }
+
+  //Condition for switching the player
+  if (randomNumber !== 1) {
+    currentScore += randomNumber;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    switchPlayer();
+  }
+});
+
+//Invoke Addeventlistener on 'Hold' button
+document.querySelector('.btn--hold').addEventListener('click', function () {
+  if (currentScore >= 100) {
+    console.log('current player wins!');
+  }
 });
